cb959192e44068d877936b68e1c8260e6c4591b7
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-05 00:35:48 +0200

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
         <div class="container flex items-center justify-center h-auto py-32 mx-auto">
 
             <div class="z-10 flex flex-col items-center px-8 xl:px-0">
-                <h1 class="mx-6 mt-1 text-xl text-5xl font-black text-center text-white lg:text-6xl sm:text-center sm:mx-0">Welcome to <br class="block lg:hidden">{{ \App\Http\Middleware\EnsureTeamMiddleware::companyFromCache()->display_name }}!</h1>
+                <h1 class="mx-6 mt-1 text-xl text-5xl font-black text-center text-white lg:text-6xl sm:text-center sm:mx-0">Welcome to <br class="block lg:hidden">Wedo!</h1>
                 <p class="w-2/3 my-6 text-xl font-normal text-center text-gray-200">Find Jobs, Employment & Career Opportunities.</p>
                 <div class="flex justify-center md:mt-10">
                     <a href="#" class="px-8 py-2 m-2 text-4xl text-center font-bold text-white bg-pink-500 border-2 border-pink-500 rounded-full hover:bg-transparent">Get Started</a>
