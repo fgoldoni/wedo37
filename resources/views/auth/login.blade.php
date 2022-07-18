0d8bc174ffedee9a9230b80e85edbadaacb98b1c
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-18 12:16:17 +0200

    initial commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,39 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <img class="w-auto h-20 fill-current text-gray-500" src="{{ app_team()->avatar }}" alt="">
-            </a>
-        </x-slot>
+<x-app-layout>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-        <!-- This example requires Tailwind CSS v2.0+ -->
-        <div class="rounded-md bg-blue-50 p-4 mb-8">
-            <div class="flex">
-                <div class="flex-shrink-0">
-                    <!-- Heroicon name: solid/information-circle -->
-                    <svg class="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <div class="ml-3 flex-1 md:flex md:justify-between">
-                    <p class="text-sm text-blue-700">Please enter your email address to get a login link.</p>
-                </div>
-            </div>
-        </div>
-
-
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
+        <!-- Section 1 -->
         <form method="POST" action="{{ route('login.link') }}">
             @csrf
+            <x-auth-session-status class="mt-4" :status="session('status')" />
+            <x-auth-validation-errors class="mt-4" :errors="$errors" />
+            <section class="w-full px-8 sm:py-24 bg-gray-100 xl:px-8">
+                <div class="max-w-5xl mx-auto">
+                    <div class="flex flex-col items-center md:flex-row">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                        <div class="hidden sm:block w-full space-y-5 md:w-3/5 md:pr-16">
+                            <p class="font-medium text-{{ app_color() }}-500 uppercase">Welcome back to {{ app_team()->display_name }} Portal</p>
+                            <h2 class="text-2xl font-extrabold leading-none text-black sm:text-3xl md:text-5xl">
+                                2,000+ companies are researching resumes on {{ app_team()->display_name }} Portal.
+                            </h2>
+                            <p class="text-xl text-gray-600 md:pr-16">In our recent survey, recruiters told us that resume search is the top tool they use to find the best candidates. Post your resume on {{ app_team()->display_name }} today to ensure recruiters and hiring managers can easily find you.</p>
+                        </div>
+                        <div class="w-full mt-16 md:mt-0 md:w-2/5">
+                            <div class="relative z-10 h-auto p-8 py-10 overflow-hidden bg-white border-b-2 border-gray-300 rounded-lg shadow-2xl px-7">
+                                <h3 class="mb-6 text-2xl font-medium text-center">Magic Login Link</h3>
+                                <input type="email" name="email" :value="old('email')" class="block w-full px-4 py-3 mb-4 border border-2 border-transparent border-none rounded-lg focus:ring focus:ring-{{ app_color() }}-500 focus:outline-none" placeholder="Email address" required autofocus>
+                                <div class="block">
+                                    <button type="submit" class="w-full px-3 py-4 font-medium text-white bg-{{ app_color() }}-600 rounded-lg">Get The Link</button>
+                                </div>
+                                <p class="w-full mt-4 text-sm text-center text-gray-500">Don't have an account? <a href="#_" class="text-{{ app_color() }}-500 underline">Sign up here</a></p>
+                            </div>
+                        </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
+                    </div>
+                </div>
+            </section>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+
+    </div>
+
+    <x-wedo.home.footer class="mt-28"></x-wedo.home.footer>
+</x-app-layout>
