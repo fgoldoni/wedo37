85913f4ed4ef88bc5cf37a03209b6bb710e8442c
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-20 01:00:47 +0200

    initial commit

--- resources/views/components/wedo/pages/layouts/heading.blade.php
+++ resources/views/components/wedo/pages/layouts/heading.blade.php
@@ -1,10 +1,13 @@
-<div {{ $attributes->merge(['class' => 'md:flex md:items-center md:justify-between pb-5 border-b border-secondary-200']) }}>
-    <div class="flex-1 min-w-0">
-        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{{ $title }}</h2>
-    </div>
-    <div class="mt-4 flex md:mt-0 md:ml-4">
-        @if(isset($action))
-            {{ $action }}
-        @endif
+<div>
+    <x-wedo.alert></x-wedo.alert>
+    <div {{ $attributes->merge(['class' => 'md:flex md:items-center md:justify-between pb-5 border-b border-secondary-200']) }}>
+        <div class="flex-1 min-w-0">
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{{ $title }}</h2>
+        </div>
+        <div class="mt-4 flex md:mt-0 md:ml-4">
+            @if(isset($action))
+                {{ $action }}
+            @endif
+        </div>
     </div>
 </div>
--- resources/views/livewire/wedo/resumes/browse.blade.php
+++ resources/views/livewire/wedo/resumes/browse.blade.php
@@ -11,11 +11,11 @@
 
             <div class="flex space-x-3">
 
+
                 <span class="shadow-sm rounded-md">
 
                     <x-wedo.input.avatar-upload wire:model="upload" id="upload" accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/pdf"></x-wedo.input.avatar-upload>
 
-                    {{ $errors->first('upload') }}
 
                 </span>
 
