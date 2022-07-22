7f02cf054799f8f8ee83860955ae419445229880
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-22 16:05:07 +0200

    initial commit

--- resources/views/components/wedo/home/features.blade.php
+++ resources/views/components/wedo/home/features.blade.php
@@ -9,7 +9,6 @@
         </div>
 
         <div class="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-
             @if(isset(app_areas()[0]) &&  $area = app_areas()[0])
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-1 sm:row-start-2 md:row-start-3">
                     <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
@@ -24,38 +23,34 @@
                     </a>
                 </div>
             @endif
-
             @if(isset(app_areas()[1]) &&  $area = app_areas()[1])
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-1 xl:col-start-2 sm:row-start-4 md:row-start-5 xl:row-start-2">
-                        <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
-                            <div class="absolute inset-0 w-full h-full">
-                                <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
-                                <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/member-management.jpeg" alt="">
-                            </div>
-                            <div class="relative z-10 flex flex-col items-start justify-start w-full px-6 py-7">
-                                <span class="px-2 py-1 mb-3 text-xs font-semibold tracking-tight text-white uppercase bg-{{ app_color() }}-500 rounded-md">{{ $area->jobs_count }} Position(s)</span>
-                                <h4 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-3xl md:text-2xl lg:text-3xl">{{ $area->name }}</h4>
-                            </div>
-                        </a>
-                    </div>
+                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                        <div class="absolute inset-0 w-full h-full">
+                            <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
+                            <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/member-management.jpeg" alt="">
+                        </div>
+                        <div class="relative z-10 flex flex-col items-start justify-start w-full px-6 py-7">
+                            <span class="px-2 py-1 mb-3 text-xs font-semibold tracking-tight text-white uppercase bg-{{ app_color() }}-500 rounded-md">{{ $area->jobs_count }} Position(s)</span>
+                            <h4 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-3xl md:text-2xl lg:text-3xl">{{ $area->name }}</h4>
+                        </div>
+                    </a>
+                </div>
             @endif
-
             @if(isset(app_areas()[2]) &&  $area = app_areas()[2])
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-2 xl:col-start-2 sm:row-start-6 md:row-start-2 xl:row-start-4">
-                        <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
-                            <div class="absolute inset-0 w-full h-full">
-                                <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
-                                <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/custom-navigation.jpeg" alt="">
-                            </div>
-                            <div class="relative z-10 flex flex-col items-start justify-start w-full px-6 py-7">
-                                <span class="px-2 py-1 mb-3 text-xs font-semibold tracking-tight text-white uppercase bg-{{ app_color() }}-500 rounded-md">{{ $area->jobs_count }} Position(s)</span>
-                                <h4 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-3xl md:text-2xl lg:text-3xl">{{ $area->name }}</h4>
-                            </div>
-                        </a>
-                    </div>
+                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                        <div class="absolute inset-0 w-full h-full">
+                            <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
+                            <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/custom-navigation.jpeg" alt="">
+                        </div>
+                        <div class="relative z-10 flex flex-col items-start justify-start w-full px-6 py-7">
+                            <span class="px-2 py-1 mb-3 text-xs font-semibold tracking-tight text-white uppercase bg-{{ app_color() }}-500 rounded-md">{{ $area->jobs_count }} Position(s)</span>
+                            <h4 class="text-4xl font-bold tracking-tight text-gray-100 sm:text-3xl md:text-2xl lg:text-3xl">{{ $area->name }}</h4>
+                        </div>
+                    </a>
+                </div>
             @endif
-
-
             @if(isset(app_areas()[3]) && $area = app_areas()[3])
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-2 xl:col-start-3 sm:row-start-1 md:row-start-4 xl:row-start-1">
                     <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
@@ -70,8 +65,6 @@
                     </a>
                 </div>
             @endif
-
-
             @if(isset(app_areas()[4]) && $area = app_areas()[4])
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-3 xl:col-start-3 sm:row-start-3 md:row-start-1 xl:row-start-3">
                     <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
@@ -86,8 +79,6 @@
                     </a>
                </div>
             @endif
-
-
             @if(isset(app_areas()[5]) && $area = app_areas()[5])
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-3 xl:col-start-4 sm:row-start-5 md:row-start-3 xl:row-start-2">
                     <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
