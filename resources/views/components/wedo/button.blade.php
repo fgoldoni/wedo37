ac25cabc6ccc1b024a59df112005ab02f99bbeb1
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-26 17:32:40 +0200

    initial commit

--- resources/views/components/wedo/button.blade.php
+++ resources/views/components/wedo/button.blade.php
@@ -7,7 +7,7 @@
 @if($link)
     <a
         href="{{ $link }}"
-        {{ $attributes->merge(['class' => 'inline-flex underline items-center whitespace-nowrap font-medium text-' . app_color() . '-700 hover:text-' . app_color() . '-500']) }}
+        {{ $attributes->merge(['class' => 'w-full flex items-center justify-center bg-' . app_color() . '-600 py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-' . app_color() . '-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-' . app_color() . '-500 sm:w-full sm:flex-grow-0']) }}
     >
         {{ $slot }}
     </a>
--- resources/views/livewire/wedo/applicants/browse-list.blade.php
+++ resources/views/livewire/wedo/applicants/browse-list.blade.php
@@ -43,36 +43,16 @@
                         </div>
                     </div>
                     <div class="mt-6 space-y-4 sm:mt-0 sm:ml-6 sm:flex-none sm:w-40">
-                        <a href="{{ route('applicants.show', $row->id) }}" class="w-full flex items-center justify-center bg-{{ app_color() }}-600 py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-{{ app_color() }}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-{{ app_color() }}-500 sm:w-full sm:flex-grow-0">
-                            {{__('Details')}}
-                        </a>
-                        @if ($row->status === 'approved')
-                            <button type="button" class="cursor-not-allowed w-full flex items-center justify-center bg-green-100 text-green-800 py-2 px-2.5 border border-gray-300 rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:w-full sm:flex-grow-0">
-                                <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-green-400" fill="currentColor" viewBox="0 0 8 8">
-                                    <circle cx="4" cy="4" r="3" />
-                                </svg>
-                                {{ $row->status }}
-                            </button>
-                        @endif
-
-                        @if ($row->status === 'pending')
-                            <button type="button" class="cursor-not-allowed w-full flex items-center justify-center bg-yellow-100 text-yellow-800 py-2 px-2.5 border border-gray-300 rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:w-full sm:flex-grow-0">
-                                <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-yellow-400" fill="currentColor" viewBox="0 0 8 8">
-                                    <circle cx="4" cy="4" r="3" />
-                                </svg>
-                                {{ $row->status }}
-                            </button>
-                        @endif
-
-                        @if ($row->status === 'rejected')
-                            <button type="button" class="cursor-not-allowed w-full flex items-center justify-center bg-rose-100 text-rose-800 py-2 px-2.5 border border-gray-300 rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500 sm:w-full sm:flex-grow-0">
-                                <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-rose-400" fill="currentColor" viewBox="0 0 8 8">
-                                    <circle cx="4" cy="4" r="3" />
-                                </svg>
-                                {{ $row->status }}
-                            </button>
-                        @endif
-
+                        <x-wedo.button link="{{ route('applicants.show', $row->id) }}">
+                            <span>{{__('Details')}}</span>
+                            <x-heroicon-o-arrow-right class="-mr-1 ml-2 h-5 w-5 text-white"/>
+                        </x-wedo.button>
+                        <x-wedo.outline-button class="cursor-not-allowed w-full items-center">
+                            <svg class="-ml-0.5 mr-1.5 h-2 w-2 @if ($row->status === 'approved') text-green-400 @elseif($row->status === 'rejected') text-rose-400 @else text-yellow-400 @endif" fill="currentColor" viewBox="0 0 8 8">
+                                <circle cx="4" cy="4" r="3" />
+                            </svg>
+                            <span class="@if ($row->status === 'approved') text-green-400 @elseif($row->status === 'rejected') text-rose-400 @else text-yellow-400 @endif">{{ $row->status }}</span>
+                        </x-wedo.outline-button>
                     </div>
                 </div>
             </div>
