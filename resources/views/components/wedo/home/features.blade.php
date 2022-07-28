3d5a58aecc4f5b7e263d5491a9a284baa4f893d0
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-28 19:21:00 +0200

    initial commit

--- resources/views/components/wedo/home/features.blade.php
+++ resources/views/components/wedo/home/features.blade.php
@@ -1,3 +1,8 @@
+@props([
+    'filters' => [
+        'categories' => []
+    ]
+])
 <section class="relative px-10 py-16 overflow-hidden bg-gray-100">
     <img class="absolute left-0 z-0 w-3/4 transform -translate-y-1/2 opacity-70 top-1/2" src="https://cdn.devdojo.com/tails/images/gradient-blob.svg">
     <img class="absolute left-0 z-0 object-cover object-center w-full h-full opacity-50 top-24" src="https://cdn.devdojo.com/tails/images/swirl-white.svg">
@@ -10,8 +15,11 @@
 
         <div class="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
             @if(isset(app_areas()[0]) &&  $area = app_areas()[0])
+                @php
+                    $filters['categories'][] = $area->id;
+                @endphp
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-1 sm:row-start-2 md:row-start-3">
-                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                    <a href="{{ route('jobs.index', ['filters' =>$filters]) }}" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
                         <div class="absolute inset-0 w-full h-full">
                             <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
                             <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/beautiful-dashboard.jpeg" alt="">
@@ -24,8 +32,11 @@
                 </div>
             @endif
             @if(isset(app_areas()[1]) &&  $area = app_areas()[1])
+                     @php
+                        $filters['categories'] = [$area->id];
+                    @endphp
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-1 xl:col-start-2 sm:row-start-4 md:row-start-5 xl:row-start-2">
-                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                    <a href="{{ route('jobs.index', ['filters' =>$filters]) }}" class="relative flex flex-col items-start justify-end w-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
                         <div class="absolute inset-0 w-full h-full">
                             <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
                             <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/member-management.jpeg" alt="">
@@ -38,8 +49,11 @@
                 </div>
             @endif
             @if(isset(app_areas()[2]) &&  $area = app_areas()[2])
+                     @php
+                        $filters['categories'] = [$area->id];
+                    @endphp
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-2 xl:col-start-2 sm:row-start-6 md:row-start-2 xl:row-start-4">
-                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                    <a href="{{ route('jobs.index', ['filters' =>$filters]) }}" class="relative flex flex-col items-start justify-end w-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
                         <div class="absolute inset-0 w-full h-full">
                             <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
                             <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/custom-navigation.jpeg" alt="">
@@ -52,8 +66,11 @@
                 </div>
             @endif
             @if(isset(app_areas()[3]) && $area = app_areas()[3])
+                     @php
+                        $filters['categories'] = [$area->id];
+                    @endphp
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-2 xl:col-start-3 sm:row-start-1 md:row-start-4 xl:row-start-1">
-                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                    <a href="{{ route('jobs.index', ['filters' =>$filters]) }}" class="relative flex flex-col items-start justify-end w-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
                         <div class="absolute inset-0 w-full h-full">
                             <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
                             <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/integration-center.jpeg" alt="">
@@ -66,8 +83,11 @@
                 </div>
             @endif
             @if(isset(app_areas()[4]) && $area = app_areas()[4])
+                     @php
+                        $filters['categories'] = [$area->id];
+                    @endphp
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-3 xl:col-start-3 sm:row-start-3 md:row-start-1 xl:row-start-3">
-                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                    <a href="{{ route('jobs.index', ['filters' =>$filters]) }}" class="relative flex flex-col items-start justify-end w-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
                         <div class="absolute inset-0 w-full h-full">
                             <div class="absolute bottom-0 left-0 z-10 w-full h-full opacity-30 bg-gradient-to-b from-transparent to-gray-900"></div>
                             <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/design-handoff.jpeg" alt="">
@@ -80,8 +100,11 @@
                </div>
             @endif
             @if(isset(app_areas()[5]) && $area = app_areas()[5])
+                    @php
+                        $filters['categories'] = [$area->id];
+                    @endphp
                 <div class="row-span-2 col-span-full sm:col-span-1 md:col-start-3 xl:col-start-4 sm:row-start-5 md:row-start-3 xl:row-start-2">
-                    <a href="#_" class="relative flex flex-col items-start justify-end w-full h-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
+                    <a href="{{ route('jobs.index', ['filters' =>$filters]) }}" class="relative flex flex-col items-start justify-end w-full overflow-hidden bg-black shadow-lg rounded-xl group" style="aspect-ratio: 1/1;">
                         <div class="absolute inset-0 w-full h-full">
                             <div class="absolute bottom-0 left-0 z-10 w-full h-full bg-gradient-to-b from-transparent to-gray-900 opacity-30"></div>
                             <img class="absolute inset-0 object-cover object-center w-full h-full transition duration-500 lg:opacity-80 group-hover:opacity-100 group-hover:scale-110" src="https://cdn.devdojo.com/images/september2021/3rd-party-modules.jpeg" alt="">
