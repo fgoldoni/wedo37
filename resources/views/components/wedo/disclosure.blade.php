ea158b3d53f50b736cd561ef79a8277e6f80df98
Goldoni <fotsa.goldoni@yahoo.fr>
2022-08-04 10:58:34 +0200

    initial commit

--- resources/views/components/wedo/disclosure.blade.php
+++ resources/views/components/wedo/disclosure.blade.php
@@ -2,22 +2,19 @@
     'title' => 'Details',
     'open' => false,
 ])
-<div {{ $attributes->merge(['class' => 'w-full mx-auto px-4 sm:px-6 lg:px-8']) }}>
+<div {{ $attributes->merge(['class' => 'border-t border-gray-200 p-6 bg-white rounded-lg shadow-md  border-2 border-secondary-200 col-span-1 sm:col-span-2']) }}>
     <h2 class="sr-only">{{ $title }}</h2>
-    <div wire:ignore x-data="{
-            open: '{{ $open }}',
-            display_name: '',
-        }">
+    <div wire:ignore x-data="{ open: '{{ $open }}' }">
         <h3>
             <!-- Expand/collapse question button -->
-            <button @click="open = !open" type="button" class="group relative w-full py-6 flex justify-between items-center text-left" aria-controls="disclosure-1" aria-expanded="false">
+            <button @click="open = !open" type="button" class="rounded-lg bg-slate-100 px-4 py-3 w-full flex items-center justify-between text-sm text-secondary-500 hover:text-secondary-600" aria-controls="disclosure-1" aria-expanded="false">
             <!-- Open: "text-{{ app_color() }}-600", Closed: "text-gray-900" -->
                 <span class="capitalize text-base font-medium text-gray-900" :class="{ 'text-{{ app_color() }}-600': open, 'text-gray-900': !(open) }"> {{ $title }} </span>
                 <span class="ml-6 flex items-center">
 
-                                              <svg class="h-6 w-6 text-gray-400 group-hover:text-gray-500 block"  :class="{ 'hidden': open, 'block': !(open) }" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                                              </svg>
+                      <svg class="h-6 w-6 text-gray-400 group-hover:text-gray-500 block"  :class="{ 'hidden': open, 'block': !(open) }" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                      </svg>
                     <!--
                       Heroicon name: outline/minus-sm
 
@@ -29,7 +26,7 @@
                     </span>
             </button>
         </h3>
-        <div class="col-span-1 sm:col-span-3" id="disclosure-1" x-show="open" x-cloak x-transition:enter="transition ease-out duration-150" x-transition:enter-start="opacity-0 transform scale-90" x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-90">
+        <div class="pt-6 col-span-1 sm:col-span-3" x-show="open" x-cloak x-transition:enter="transition duration-100 ease-out" x-transition:enter-start="transform scale-95 opacity-0" x-transition:enter-end="transform scale-100 opacity-100" x-transition:leave="transition duration-75 ease-out" x-transition:leave-start="transform scale-100 opacity-100" x-transition:leave-end="transform scale-95 opacity-0">
             {{ $slot }}
         </div>
     </div>
--- resources/views/livewire/wedo/applicants/item.blade.php
+++ resources/views/livewire/wedo/applicants/item.blade.php
@@ -74,8 +74,8 @@
                             <div class="grid grid-cols-1 gap-6">
                                 <div class="col-span-1">
                                     <dt class="text-sm font-medium text-gray-900">{{ __('Job Description') }}</dt>
-                                    <dd class="mt-1 max-w-prose text-base text-gray-600 text-justify space-y-5">
-                                        {{ $job->content }}
+                                    <dd class="whitespace-pre-line mt-1 max-w-prose text-base text-gray-600 text-justify space-y-5">
+                                        {!! $job->content !!}
                                     </dd>
                                 </div>
 
