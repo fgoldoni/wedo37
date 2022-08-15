8b7a579ce762fe7bff0a0b93442958be985f6272
Goldoni <fotsa.goldoni@yahoo.fr>
2022-08-15 02:59:25 +0200

    initial commit

--- resources/views/livewire/wedo/modals/popup/add.blade.php
+++ resources/views/livewire/wedo/modals/popup/add.blade.php
@@ -18,10 +18,10 @@
         </div>
         <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
             <a href="{{ route('carts.index') }}" wire:click="$emit('closeModal')" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-{{ app_color() }}-600 text-base font-medium text-white hover:bg-{{ app_color() }}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-{{ app_color() }}-500 sm:col-start-2 sm:text-sm">
-                View Shopping Cart
+                Go to Basket
             </a>
             <button type="button" wire:click="$emit('closeModal')" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-{{ app_color() }}-500 sm:mt-0 sm:col-start-1 sm:text-sm">
-                Continue Shopping
+                Continue Browse
             </button>
         </div>
     </div>
