344a440e19188c5150eabeb8bdae6b67d10a3569
Goldoni <fotsa.goldoni@yahoo.fr>
2022-08-20 16:58:22 +0200

    initial commit

--- resources/views/components/wedo/home/hero.blade.php
+++ resources/views/components/wedo/home/hero.blade.php
@@ -26,7 +26,7 @@
                 <x-heroicon-o-location-marker class="-ml-1 mr-2 h-6 w-6"/>
                 {{ app_event()?->address }}
             </p>
-            <div class="uppercase flex flex-col items-center pt-2 sm:flex-row sm:pt-4 space-x-6  mb-5">
+            <div class="uppercase flex items-center pt-2 sm:flex-row sm:pt-4 space-x-1  mb-5">
                 <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-gray-100 text-gray-800">
                   <svg class="-ml-1 mr-1.5 h-2 w-2 text-gray-400" fill="currentColor" viewBox="0 0 8 8">
                     <circle cx="4" cy="4" r="3" />
