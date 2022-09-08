b94b6aefd3095094c3e6def518da2d91b770bdf4
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-08 21:34:41 +0200

    initial commit

--- app/Http/Controllers/Wedo/PaymentsController.php
+++ app/Http/Controllers/Wedo/PaymentsController.php
@@ -2,11 +2,25 @@
 namespace App\Http\Controllers\Wedo;
 
 use App\Http\Controllers\Controller;
+use App\Http\Services\Contracts\ApiInterface;
 
 class PaymentsController extends Controller
 {
     public function index()
     {
+        return view('wedo.payments.index');
+    }
+
+    public function success()
+    {
+        try {
+            $response = app()->make(ApiInterface::class)->post('/paypal', [
+                'authorizationId' => $authorizationId,
+            ]);
+        } catch (\Exception $e) {
+            session()->flash('error', $e->getMessage() != '' ? $e->getMessage() : 'Unable to process payment');
+            return $this->redirectRoute('payments.index');
+        }
         return view('wedo.payments.index');
     }
 }
