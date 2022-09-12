e579e23570118dad7ced8396d945b537bcaa4add
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-12 19:54:52 +0200

    initial commit

--- resources/views/livewire/wedo/home/partners.blade.php
+++ resources/views/livewire/wedo/home/partners.blade.php
@@ -1,6 +1,6 @@
 <!-- Section 1 -->
 <section class="w-full py-16 bg-white lg:py-24">
-    <div class="mx-auto text-center max-w-7xl">
+    <div class="relative w-full px-5 py-10 mx-auto sm:py-12 md:py-16 md:px-10 max-w-7xl">
         <div class="space-y-12 md:text-left">
             <div class="mb-20 space-y-5 sm:mx-auto sm:max-w-xl md:max-w-2xl sm:space-y-4 lg:max-w-5xl">
                 <h2 class="relative text-4xl font-extrabold tracking-tight sm:text-5xl">
