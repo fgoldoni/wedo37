10282d7bff43aac940dd51f21041f88e371e9a72
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-12 21:29:06 +0200

    initial commit

--- resources/views/components/wedo/orders/item.blade.php
+++ resources/views/components/wedo/orders/item.blade.php
@@ -148,7 +148,7 @@
                                                                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
                                                                         </svg>
                                                                     </div>
-                                                                    <p class="ml-3 text-sm text-gray-700">{{ $tag->name }}</p>
+                                                                    <p class="ml-3 text-sm text-gray-700">{{ $tag }}</p>
                                                                 </li>
                                                             @endforeach
                                                         </ul>
