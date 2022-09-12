9862aea4993318ef2df75878c4fd12dbfa901603
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-12 19:58:19 +0200

    initial commit

--- resources/views/livewire/wedo/home/package.blade.php
+++ resources/views/livewire/wedo/home/package.blade.php
@@ -11,7 +11,7 @@
                     </a>
                     <div class="relative z-20 w-full h-auto py-8 text-white bg-purple-500 border-t-0 border-yellow-200 px-7">
                         <a href="{{ route('tickets.show', $ticket->id) }}" class="hover:scale-125 ease-in-out duration-150 inline-block text-xs font-semibold absolute top-0 -mt-3.5 rounded-full px-4 py-2 uppercase text-purple-500 bg-white">{{ __('Details') }}</a>
-                        <h2 class="uppercase mb-5 text-5xl font-bold"><a href="#_">{{ Str::limit($ticket->name, 18, '...') }}</a></h2>
+                        <h2 class="uppercase mb-5 text-2xl font-bold"><a href="#_">{{ Str::limit($ticket->name, 18, '...') }}</a></h2>
                         <p class="mb-2 text-lg font-normal text-purple-100 opacity-100">FROM {{ $ticket->price }} Euro</p>
                     </div>
                 </div>
