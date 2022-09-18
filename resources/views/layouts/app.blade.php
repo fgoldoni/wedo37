e488c6e49d06f390cf2c7dd9a904b79a663e69d3
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-18 23:15:48 +0200

    initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" href="{{ app_team_avatar() }}" type="image/png" sizes="16x16"><
+        <link rel="icon" href="{{ app_team_avatar() }}" type="image/png" sizes="16x16">
         {!! SEO::generate() !!}
         <!-- Fonts -->
         <link href='https://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet'>
