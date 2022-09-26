e8c87f25abaaacaa517b541fa211e26d22449e56
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-26 17:38:06 +0200

    initial commit

--- resources/views/components/wedo/home/footer.blade.php
+++ resources/views/components/wedo/home/footer.blade.php
@@ -1,44 +1,50 @@
-<footer {{ $attributes->merge(['class' => 'bg-gray-900']) }} aria-labelledby="footer-heading">
+<footer class="bg-gray-100" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div class="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
-            <div class="col-span-1 xl:col-span-4">
-                <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div class="grid grid-cols-2 gap-8 xl:col-span-4">
+                <div class="md:grid md:grid-cols-1 md:gap-8">
                     <div>
-                        <h3 class="text-base font-medium text-white">{{ __('Informations') }}</h3>
+                        <h3 class="text-base font-medium text-gray-900">{{ __('Informations') }}</h3>
                         <ul role="list" class="mt-4 space-y-4">
                             <li>
-                                <a href="javascript:;" class="text-base text-gray-300 hover:text-white">{{ __('UPDATE-SEITE-COVID-19') }}</a>
+                                <a href="javascript:;" class="text-base text-gray-500 hover:text-gray-900">{{ __('UPDATE-SEITE-COVID-19') }}</a>
                             </li>
 
                             <li>
-                                <a href="javascript:;" class="text-base text-gray-300 hover:text-white">{{ __('À propos') }}</a>
+                                <a href="javascript:;" class="text-base text-gray-500 hover:text-gray-900">{{ __('À propos') }}</a>
                             </li>
 
                             <li>
-                                <a href="{{ route('contact') }}" class="text-base text-gray-300 hover:text-white">{{ __('Questions les plus fréquentes (F.A.Q )') }}</a>
+                                <a href="javascript:;" class="text-base text-gray-500 hover:text-gray-900">{{ __('Questions les plus fréquentes (F.A.Q )') }}</a>
+                            </li>
+
+                            <li>
+                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">Insights</a>
                             </li>
                         </ul>
                     </div>
-                    <div class="mt-12 md:mt-0">
-                        <h3 class="text-base font-medium text-white">{{ __('Mentions légales') }}</h3>
+                </div>
+                <div class="md:grid md:grid-cols-2 md:gap-8">
+                    <div>
+                        <h3 class="text-base font-medium text-gray-900">{{ __('Mentions légales') }}</h3>
                         @livewire('wedo.home.footer')
                     </div>
                 </div>
             </div>
             <div class="mt-12 xl:mt-0">
-                <h3 class="text-base font-medium text-white">Language</h3>
+                <h3 class="text-base font-medium text-gray-900">Language</h3>
                 <form class="mt-4 sm:max-w-xs">
                     <fieldset class="w-full">
                         <label for="language" class="sr-only">Language</label>
                         <div class="relative">
-                            <select id="language" name="language" class="block w-full appearance-none rounded-md border border-transparent bg-gray-700 bg-none py-2 pl-3 pr-10 text-base text-white focus:border-white focus:outline-none focus:ring-white sm:text-sm">
+                            <select id="language" name="language" class="block w-full appearance-none rounded-md border border-gray-300 bg-white bg-none py-2 pl-3 pr-10 text-base text-gray-900 focus:border-{{ app_color() }}-500 focus:outline-none focus:ring-{{ app_color() }}-500 sm:text-sm">
                                 <option selected>French</option>
                                 <option>English</option>
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
                                 <!-- Heroicon name: mini/chevron-down -->
-                                <svg class="h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
                             </div>
@@ -47,23 +53,23 @@
                 </form>
             </div>
         </div>
-        <div class="border-t border-gray-700 pt-8 lg:flex lg:items-center lg:justify-between xl:mt-0">
+        <div class="border-t border-gray-200 pt-8 lg:flex lg:items-center lg:justify-between xl:mt-0">
             <div>
-                <h3 class="text-base font-medium text-white">S'abonner à notre newsletter</h3>
-                <p class="mt-2 text-base text-gray-300">{{ __('pages/home.footer.copyright', ['team' => app_team_name()]) }}</p>
+                <h3 class="text-base font-medium text-gray-900">S'abonner à notre newsletter</h3>
+                <p class="mt-2 text-base text-gray-500">Les dernières nouvelles et événements les plus récents, envoyés dans votre boîte de réception chaque semaine.</p>
             </div>
             <form class="mt-4 sm:flex sm:max-w-md lg:mt-0">
                 <label for="email-address" class="sr-only">Email address</label>
-                <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border border-transparent bg-white py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:border-white focus:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 sm:max-w-xs" placeholder="Enter your email">
+                <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border border-gray-300 bg-white py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:border-{{ app_color() }}-500 focus:placeholder-gray-400 focus:outline-none focus:ring-{{ app_color() }}-500 sm:max-w-xs" placeholder="Enter your email">
                 <div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-                    <button type="submit" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800">Subscribe</button>
+                    <button type="submit" class="flex w-full items-center justify-center rounded-md border border-transparent bg-{{ app_color() }}-600 py-2 px-4 text-base font-medium text-white hover:bg-{{ app_color() }}-700 focus:outline-none focus:ring-2 focus:ring-{{ app_color() }}-500 focus:ring-offset-2">Subscribe</button>
                 </div>
             </form>
         </div>
-        <div class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
+        <div class="mt-8 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between">
             <div class="flex space-x-6 md:order-2">
                 @if(app_team()->facebook)
-                    <a href="{{ app_team()->facebook }}" target="_blank"  class="text-gray-400 hover:text-gray-300">
+                    <a href="{{ app_team()->facebook }}" target="_blank"  class="text-gray-400 hover:text-gray-500">
                         <span class="sr-only">Facebook</span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
@@ -71,7 +77,7 @@
                     </a>
                 @endif
                 @if(app_team()->instagram)
-                    <a href="{{ app_team()->instagram }}" class="text-gray-400 hover:text-gray-300">
+                    <a href="{{ app_team()->instagram }}" class="text-gray-400 hover:text-gray-500">
                         <span class="sr-only">Instagram</span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
@@ -80,7 +86,7 @@
                 @endif
 
                 @if(app_team()->twitter)
-                    <a href="{{ app_team()->twitter }}" class="text-gray-400 hover:text-gray-300">
+                    <a href="{{ app_team()->twitter }}" class="text-gray-400 hover:text-gray-500">
                         <span class="sr-only">Twitter</span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -89,13 +95,14 @@
                 @endif
 
                 @if(app_team()->youtube)
-                    <a href="{{ app_team()->youtube }}" class="text-gray-400 hover:text-gray-300">
+                    <a href="{{ app_team()->youtube }}" class="text-gray-400 hover:text-gray-500">
                         <span class="sr-only">Youtube</span>
                         <svg class="h-6 w-6 fill-current" viewBox="0 0 80 56" xmlns="http://www.w3.org/2000/svg"><path d="m41.771 55.637-16.395-.3c-5.308-.104-10.63.104-15.834-.978-7.916-1.618-8.477-9.548-9.064-16.2-.809-9.35-.496-18.872 1.03-28.145.862-5.204 4.252-8.309 9.495-8.647C28.703.141 46.52.287 64.18.86c1.865.052 3.743.339 5.582.665 9.078 1.591 9.299 10.578 9.888 18.143.586 7.642.339 15.325-.783 22.915-.9 6.285-2.622 11.556-9.888 12.065-9.104.665-18 1.2-27.129 1.03 0-.04-.052-.04-.078-.04Zm-9.638-15.912c6.86-3.94 13.59-7.813 20.411-11.725-6.873-3.94-13.59-7.813-20.411-11.726v23.451Z" fill-rule="nonzero"></path></svg>
                     </a>
                 @endif
             </div>
-            <p class="mt-8 text-base text-gray-400 md:order-1 md:mt-0"> {{ __('pages/home.footer.description') }}</p>
+            <p class="mt-8 text-base text-gray-400 md:order-1 md:mt-0">{{ __('pages/home.footer.copyright', ['team' => app_team_name()]) }}</p>
         </div>
     </div>
 </footer>
+
--- resources/views/livewire/wedo/home/footer.blade.php
+++ resources/views/livewire/wedo/home/footer.blade.php
@@ -1,7 +1,7 @@
 <ul role="list" class="mt-4 space-y-4">
     @forelse ($rows as $row)
         <li>
-            <a href="{{ $row->url }}" class="text-base text-gray-300 hover:text-white">{{ $row->name }}</a>
+            <a href="{{ $row->url }}" class="text-base text-gray-500 hover:text-gray-900">{{ $row->name }}</a>
         </li>
     @empty
     @endforelse
