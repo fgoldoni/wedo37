b96577158fd2639aa9b22db931ba130daa48378c
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-26 17:54:19 +0200

    initial commit

--- resources/views/livewire/wedo/home/footer.blade.php
+++ resources/views/livewire/wedo/home/footer.blade.php
@@ -1,8 +1,9 @@
 <ul role="list" class="mt-4 space-y-4">
-    @forelse ($rows as $row)
-        <li>
-            <a href="{{ $row->url }}" class="text-base text-gray-500 hover:text-gray-900">{{ $row->name }}</a>
-        </li>
-    @empty
-    @endforelse
+    @if(count($rows))
+        @foreach ($rows as $row)
+            <li>
+                <a href="{{ $row->url }}" class="text-base text-gray-500 hover:text-gray-900">{{ $row->name }}</a>
+            </li>
+        @endforeach
+    @endif
 </ul>
