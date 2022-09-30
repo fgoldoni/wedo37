2a6bf68e22ba430d6dd9a137c7f96e26ae1d88d4
Goldoni <fotsa.goldoni@yahoo.fr>
2022-09-30 11:18:59 +0200

    initial commit

--- app/Http/Controllers/Wedo/TicketsController.php
+++ app/Http/Controllers/Wedo/TicketsController.php
@@ -3,7 +3,9 @@ namespace App\Http\Controllers\Wedo;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ticket;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\SEOTools;
+use Illuminate\Support\Str;
 
 class TicketsController extends Controller
 {
@@ -26,13 +28,19 @@ class TicketsController extends Controller
 
     public function show(Ticket $ticket)
     {
-        SEOTools::setTitle($ticket->name . ' - ' . app_team_name(), false);
+        SEOTools::setTitle( Str::upper($ticket->name)  . ' - ' . app_team_name(), false);
         SEOTools::setDescription($ticket->description);
         SEOTools::opengraph()->setUrl(route('tickets.show', $ticket->id));
+        SEOTools::opengraph()->addImage($ticket->avatar_url, ['height' => 300, 'width' => 300]);
+        SEOTools::opengraph()->addProperty('type', 'website');
+        SEOTools::opengraph()->addProperty('locale', app()->getLocale());
+        SEOTools::opengraph()->addProperty('locale:alternate', ['de', 'en-us']);
         SEOTools::setCanonical(url('/'));
-        SEOTools::opengraph()->addProperty('type', 'articles');
+        SEOTools::twitter()->setTitle(Str::upper($ticket->name)  . ' - ' . app_team_name());
         SEOTools::twitter()->setSite('@LuizVinicius73');
-        SEOTools::jsonLd()->addImage(app_team_avatar());
+        SEOTools::jsonLd()->setTitle(Str::upper($ticket->name)  . ' - ' . app_team_name());
+        SEOTools::jsonLd()->addImage($ticket->avatar_url, ['height' => 300, 'width' => 300]);
+        SEOMeta::addKeyword(['billets', 'events', 'tickets']);
 
         return view('wedo.tickets.item', compact('ticket'));
     }
