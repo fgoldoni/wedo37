684e26a2927d138d9e96fb6186f6f3c10c2cc388
Goldoni <fotsa.goldoni@yahoo.fr>
2022-10-24 12:27:39 +0200

    initial commit

--- resources/views/wedo/payments/success.blade.php
+++ resources/views/wedo/payments/success.blade.php
@@ -4,6 +4,27 @@
 
         <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
             <div class="lg:col-span-3">
+                <div class="bg-white shadow sm:rounded-lg">
+                    <div class="px-4 py-5 sm:p-6">
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">
+                            {{ __('Congratulations! Your order has been registered') }}
+                        </h3>
+                        <div class="mt-2 max-w-xl text-sm text-gray-500">
+                            <p>
+                                {{ __('It is confirmed and will be processed immediately. You will receive a confirmation email when your order is complete.') }}
+                            </p>
+                        </div>
+                        <div class="mt-3 text-sm">
+                            <a href="#" class="font-medium text-{{ app_color() }}-600 hover:text-{{ app_color() }}-500">
+                                {{ __('layout.navigation.browse_orders') }}
+                                <span aria-hidden="true"> &rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="lg:col-span-3">
                 <h2 class="btn-title text-3xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-{{ app_color() }}-500 to-{{ app_color() }}-900">{{ app_event()?->name }}</h2>
 
                 <div class="mt-2 border-b border-gray-200 pb-5 text-sm sm:flex sm:justify-between">
@@ -105,93 +126,83 @@
                         {{ app_event()->phone }}
                     </p>
                 </div>
-
-
-{{--                <div class="flex flex-col gap-1">--}}
-{{--                    <p class="font-medium">Tax Number:</p>--}}
-{{--                    <p class="text-gray-400 text-sm">--}}
-{{--                        TAX99110212--}}
-{{--                    </p>--}}
-{{--                </div>--}}
             </div>
         </div>
 
-        <!--items -->
-        <div class="w-full bg-gray-50 py-4">
-            <div class="max-w-5xl mx-auto grid sm:grid-cols-2 gap-10 px-4">
-                <div class="flex flex-row items-center justify-start">
-                    <p class="uppercase font-medium">{{ __('Item(s)') }}</p>
-                </div>
+        <div class="bg-white shadow sm:rounded-lg">
+            <div class="px-4 py-5 sm:p-6">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">
+                    {{ __('layout.card.summary') }}
+                </h3>
+                <div class="mt-2 w-full text-sm text-gray-500">
+                    @if(app_session_order())
+                    <ul role="list" class="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-gray-500">
+                        @foreach(app_session_order()?->items as $item)
+                            @if($item->associatedModel === \App\Models\Ticket::$apiModel)
+                                <li class="flex space-x-6 py-6">
+                                    <img src="{{ $item->attributes->avatar_url }}" alt="{{ $item->attributes->name }}" class="btn-img h-24 w-24 flex-none rounded-md bg-gray-100 object-cover object-center">
+                                    <div class="flex-auto space-y-1">
+                                        <h3 class="text-gray-900">
+                                            <a href="javascript:;" class="uppercase btn-title truncate font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-{{ $item->attributes->color }}-400 to-{{ $item->attributes->color }}-900">{{ $item->quantity }} x {{ $item->name }}</a>
+                                        </h3>
+                                        <ul class="pt-2 space-y-3">
+                                            @php
+                                                $tags = is_array($item->attributes->tags) ? $item->attributes->tags : json_decode($item->attributes->tags);
+                                            @endphp
+                                            <li class="flex font-medium text-gray-500">
+                                                <x-heroicon-s-check-circle class="w-6 h-6 mr-1.5 text-{{ $item->attributes->color }}-400"></x-heroicon-s-check-circle>
+                                                {{ $item->attributes->attendees }} Person(s)
+                                            </li>
 
-                <div class="grid grid-cols-3">
-                    <p class="uppercase font-medium">{{ __('Name') }}</p>
-                    <p class="uppercase font-medium">{{ __('Unit') }}</p>
-                    <p class="uppercase font-medium">{{ __('Price') }}</p>
-                </div>
-            </div>
-        </div>
+                                            @foreach($tags as $tag)
+                                                <li class="flex items-center font-medium text-gray-500">
+                                                    <x-heroicon-s-check-circle class="w-4 h-4 mr-1.5 text-{{ $item->attributes->color }}-400"></x-heroicon-s-check-circle>
+                                                    {{ $tag }}
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    <p class="flex-none font-medium text-gray-900">{{ $item->quantity }} * € {{ $item->price }}</p>
+                                </li>
+                            @endif
+                        @endforeach
 
-        <div class="max-w-5xl mx-auto grid sm:grid-cols-2 gap-10 py-10 px-4">
+                            @foreach(app_session_order()?->items as $item)
+                                @if($item->associatedModel === \App\Models\Extra::$apiModel)
+                                    <li class="flex space-x-6 py-6">
+                                        <div class="flex-auto space-y-1">
+                                            <h3 class="text-gray-900">
+                                                <a href="javascript:;" class="uppercase btn-title truncate font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-{{ app_color() }}-400 to-{{ app_color() }}-900">{{ $item->quantity }} x {{ $item->name }}</a>
+                                            </h3>
+                                        </div>
+                                        <p class="flex-none font-medium text-gray-900">{{ $item->quantity }} * € {{ $item->price }}</p>
+                                    </li>
+                                @endif
+                            @endforeach
+                    </ul>
 
-            <div class="flex flex-row items-center justify-start">
-{{--                <div class="flex flex-col gap-4">--}}
-{{--                    <p class="font-medium">Monthly membership</p>--}}
-{{--                    <p class="font-medium">Tax VAT</p>--}}
-{{--                </div>--}}
-            </div>
-
-            <div class="flex flex-col items-start justify-start gap-4">
-                @if(app_session_order())
-                    @foreach(app_session_order()?->items as $item)
-                        <div class="grid grid-cols-3 w-full">
-                            <p class="">{{ $item->quantity }} x {{ $item->name }}</p>
-                            <p class="">€ {{ $item->price }}</p>
-                            <p class="">€ {{ $item->quantity *  $item->price}}</p>
+                    <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
+                        <div class="flex justify-between">
+                            <dt>{{ __('Subtotal') }}</dt>
+                            <dd class="text-gray-900">€ {{ app_session_order()?->sub_total }}</dd>
                         </div>
-                    @endforeach
 
-                        <div class="grid grid-cols-3 w-full border-t-2 border-gray-900 pt-4">
-                            <p class="col-span-2">{{ __('Subtotal') }}</p>
-                            <p class="">€ {{ app_session_order()?->sub_total }}</p>
+                        <div class="flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900">
+                            <dt class="text-base">{{ __('Total') }}</dt>
+                            <dd class="text-base">€ {{ app_session_order()?->total }}</dd>
                         </div>
-
-{{--                        <div class="grid grid-cols-3 w-full">--}}
-{{--                            <p class="col-span-2">Discount</p>--}}
-{{--                            <p class="">$39</p>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="grid grid-cols-3 w-full">--}}
-{{--                            <p class="col-span-2">Paid</p>--}}
-{{--                            <p class="">$149</p>--}}
-{{--                        </div>--}}
-
-                        <div class="grid grid-cols-3 w-full border-t-2 border-gray-900 pt-4">
-                            <p class="col-span-2">{{ __('Total') }}</p>
-                            <p class="">€ {{ app_session_order()?->total }}</p>
-                        </div>
-                @endif
-
-            </div>
-
-        </div>
-
-        <!--payment -->
-        <div class="max-w-5xl mx-auto grid sm:grid-cols-2 px-4 pb-10">
-            <div class="flex flex-col gap-4">
-{{--                <div class="flex flex-col gap-1">--}}
-{{--                    <p class="font-medium">Payment Method:</p>--}}
-{{--                    <p class="text-gray-400 text-sm">--}}
-{{--                        Stripe--}}
-{{--                    </p>--}}
-{{--                </div>--}}
-                <div class="flex flex-col gap-1">
-                    <p class="font-medium">Notes:</p>
-                    <p class="text-gray-400 text-sm">
-                        {{ __('We appreciate your order, we’re currently processing it. So hang tight and we’ll send you confirmation very soon!') }}
-                    </p>
+                    </dl>
+                    @endif
+                    <div class="mt-16 border-t border-gray-200 py-6 text-right">
+                        <a href="{{ route('tickets.index') }}" class="text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                            {{ __('layout.navigation.browse_tickets') }}
+                            <span aria-hidden="true"> &rarr;</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
+
 
 
     </main>
