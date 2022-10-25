ae507822a0dd7c927347e71a0c4db85e1d0682dd
Goldoni <fotsa.goldoni@yahoo.fr>
2022-10-25 13:44:00 +0200

    initial commit

--- resources/views/impressum.blade.php
+++ resources/views/impressum.blade.php
@@ -6,7 +6,7 @@
     <div class="max-w-7xl mx-auto px-4 py-6 sm:px-6 sm:py-8 lg:px-8 bg-white shadow sm:rounded-lg">
         <div class="px-4 py-5 sm:p-6">
             <div class="mt-2 w-full text-sm text-gray-500 whitespace-pre-line">
-                <meta http-equiv="content-type" content="text/html; charset=utf-8"><strong>Angaben gemäß §&nbsp;14 Absatz 1 Satz 1&nbsp;</strong><br><br><span>Sell - First&nbsp;</span><br><span>Goldoni Fouotsa&nbsp;</span><br><span>Niendorfer Straße 43, 22529 Hamburg, Deutschland&nbsp;</span><br><br><strong>Vertreten durch:</strong><span>&nbsp;</span><br><span>Goldoni Fouotsa&nbsp;</span><br><br><strong>Kontakt:</strong><span>&nbsp;</span><br><span>Telefon: +491738779485&nbsp;</span><br><span>E-Mail: contact@sell-first.com&nbsp;</span><br><br><strong>Umsatzsteuer-ID: 99999999999</strong>
+                <meta http-equiv="content-type" content="text/html; charset=utf-8"><strong>Angaben gemäß §&nbsp;14 Absatz 1 Satz 1&nbsp;</strong><br><br><span>Sell - First&nbsp;</span><br><span>Goldoni Fouotsa&nbsp;</span><br><span>Niendorfer Straße 43, 22529 Hamburg, Deutschland&nbsp;</span><br><br><strong>Vertreten durch:</strong><span>&nbsp;</span><br><span>Goldoni Fouotsa&nbsp;</span><br><br><strong>Kontakt:</strong><span>&nbsp;</span><br><span>Telefon: +491738779485&nbsp;</span><br><span>E-Mail: contact@sell-first.com&nbsp;</span><br><br><strong>USt-ID-Nr.: DE 332972034</strong>
             </div>
         </div>
     </div>
