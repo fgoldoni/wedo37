0af03bab5dbdc33ab473008a099c9fc522b76098
Goldoni <fotsa.goldoni@yahoo.fr>
2022-11-09 10:22:50 +0100

    initial commit

--- resources/views/livewire/wedo/confirmation/browse.blade.php
+++ resources/views/livewire/wedo/confirmation/browse.blade.php
@@ -5,4 +5,24 @@
         <x-wedo.orders.item :order="$order"></x-wedo.orders.item>
     </div>
 
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div class="bg-white shadow sm:rounded-lg">
+            <div class="px-4 py-5 sm:p-6">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">Conditions de paiement & d'annulation</h3>
+                <div class="mt-2 max-w-xl text-sm text-gray-500">
+                    <p>
+                        Toutes les transactions de billets se font entre un Organisateur et les Consommateurs respectifs. Par conséquent, les Consommateurs doivent contacter l'Organisateur concerné pour toute demande de remboursement. Vous trouverez de l'aide pour obtenir un remboursement ici.
+                    </p>
+                </div>
+                <div class="mt-3 text-sm">
+                    <a href="{{ route('refund') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                        Voir les détails
+                        <span aria-hidden="true"> &rarr;</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </main>
