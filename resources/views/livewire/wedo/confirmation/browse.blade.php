4a07e5be9159dd402f679c0ef2641ef97788d69d
Goldoni <fotsa.goldoni@yahoo.fr>
2022-11-09 11:48:12 +0100

    initial commit

--- resources/views/livewire/wedo/confirmation/browse.blade.php
+++ resources/views/livewire/wedo/confirmation/browse.blade.php
@@ -27,7 +27,7 @@
     </div>
 
     <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
-        <div class="lg:col-span-3 px-4 sm:px-0 my-6">
+        <div class="lg:col-span-3 px-4 sm:px-0 mb-8">
             <div class="flex items-center justify-center">
                 <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Niendorfer%20Strasse%2043&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br><style>.mapouter{position:relative;text-align:right;height:400px;width:100%;}</style><a href="https://www.embedgooglemap.net">google embed</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:100%;}</style></div></div>
             </div>
