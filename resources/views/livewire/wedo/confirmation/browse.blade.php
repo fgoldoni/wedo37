f45e8f2b86c8c62417487fac53935cdf1281c39c
Goldoni <fotsa.goldoni@yahoo.fr>
2022-11-09 10:42:55 +0100

    initial commit

--- resources/views/livewire/wedo/confirmation/browse.blade.php
+++ resources/views/livewire/wedo/confirmation/browse.blade.php
@@ -16,12 +16,22 @@
                         </p>
                     </div>
                     <div class="mt-3 text-sm">
-                        <a href="{{ route('refund') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                        <a href="{{ route('refund') }}" target="_blank" class="font-medium text-indigo-600 hover:text-indigo-500">
                             Voir les détails
                             <span aria-hidden="true"> &rarr;</span>
                         </a>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div class="lg:col-span-3 px-4 sm:px-0 my-8">
+            <div class="flex items-center justify-center">
+
+                <div class="mapouter"><div class="gmap_canvas"><iframe width="800" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Niendorfer%20Strasse%2043&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br><style>.mapouter{position:relative;text-align:right;height:400px;width:800px;}</style><a href="https://www.embedgooglemap.net">google embed</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:800px;}</style></div></div>
+
             </div>
         </div>
     </div>
